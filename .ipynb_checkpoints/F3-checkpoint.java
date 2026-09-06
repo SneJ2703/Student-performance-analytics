c671fbcceadb56de9e56d5e98cf546fd4e1bc390
Sneha <snehapatra005@gmail.com>
2026-09-06 21:37:09 +0530

    feat: complete student performance analytics and predictive pipeline

---
+++ .ipynb_checkpoints/F3-checkpoint.java
@@ -0,0 +1,14 @@
+/**
+ * WAP IN JAVA TO DISPLAY ALL THE EVEN NUMBERS FROM 10 TO 50
+ */
+public class F3
+{
+    public static void main()
+    {
+        int i;
+        for(i=10;i<=50;i=i+2)
+    {
+        System.out.print(i+" ");
+    }
+    }
+}
---
+++ .ipynb_checkpoints/s1-checkpoint.java
@@ -0,0 +1,12 @@
+// WAP TO PRINT THE SERIES    1 4  9 16 ...........100.
+public class s1
+{
+    public static void main()
+    {
+        int i;
+        for(i=1;i<=10;i++)
+        {
+            System.out.print((i*i)+" ");
+        }
+    }
+}
